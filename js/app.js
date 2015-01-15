dd93d5741fddffc8b301def3cc46312edb07f929
Greg Van Buren <g.vanburen@gmail.com>
2015-01-14 20:27:09 -0800

    Cleaner javascript

--- js/app.js
+++ js/app.js
@@ -1,6 +1,49 @@
 
 $(document).ready(function(){
 	
+	var randomNumber = Math.round(Math.random() * 100);
+	console.log(randomNumber);
+	var userGuessCounter = 0
+
+	$('#guessButton').click(function(){
+		var userGuess = $('#userGuess').val();
+		if (isNaN(userGuess)) {
+			alert('Please enter a valid number');
+		} else {
+			guessCounter();
+			letsPlay();
+		}
+	});
+
+	function guessCounter() {
+		userGuessCounter += 1;
+		$('#count').html(userGuessCounter);
+	};
+
+	function letsPlay() {
+		var userGuess = $('#userGuess').val();
+		var prevGuess = $('<li>' + userGuess + '</li>');
+		var feedback = function(guessFB) {$('#feedback').html(guessFB)};
+
+		$('#guessList').append(prevGuess);
+
+		if (userGuess == randomNumber) {
+ 			feedback("You got it!");
+ 		} else if (Math.abs(randomNumber - userGuess) <= 10) {
+ 			feedback("You're hot");
+ 		} else if (Math.abs(randomNumber - userGuess) <= 20) {
+ 			feedback("You're warm");
+ 		} else if (Math.abs(randomNumber - userGuess) <= 30) {
+ 			feedback("You're cold");
+ 		} else {
+ 			feedback("You're freezing!")
+ 		}
+ 	};
+
+	$('.new').click(function(){
+		letsPlay();
+	})
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,56 +55,7 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	$('.new').click(function(){
-  		newGame();
-  	});
 });
 
-function newGame() {
-	var randomNumber = Math.random();
-		randomNumber *= 100;
-		randomNumber = Math.round(randomNumber);
-	var userGuessCounter = 0;
-	
-	$('#count').html(userGuessCounter);
-	$('#userGuess').val('');
-	$('#guessList').html('');
-
-	$('#guessButton').click(function(){
-		var newGuess = $('#userGuess').val();
-		if (isNaN(newGuess)) {
-			alert('Please enter a valid number');
-		} else {
-			checkYourGuess();
-		}
-	});
-
-	function checkYourGuess(){
-		var userGuess = $('#userGuess').val();
-			userGuess = +userGuess;
-		var prevGuess = $('<li>' + userGuess + '</li>')
-
-		userGuessCounter++;
-		$('#userGuess').val('');
-		$('#count').html(userGuessCounter);
-		$('#guessList').append(prevGuess);
-  		
-  		if (userGuess == randomNumber) {
-  			alert("You got it!");
-  			$('#userGuess').val('');
-			$('#guessList').html('');
-			newGame();
-  		} else {
-  		// if (userGuess != randomNumber) {
-			if (Math.abs(randomNumber - userGuess) <= 10) {
-				alert("You're hot");
-			} else if (Math.abs(randomNumber - userGuess) <= 20) {
-				alert("You're warm");
-			} else {
-				alert("You're cold");
-			}
-		};
-	};	
-};
 
 
